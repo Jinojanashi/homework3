587e474a2430c605da67e0d7e186585eb3a803f2
Jinojanashi <31338808+Jinojanashi@users.noreply.github.com>
2018-04-25 14:00:06 +0700

    Add files via upload

--- app/controllers/EventController.php
+++ app/controllers/EventController.php
@@ -1,56 +1,59 @@
 <?php
 use Phalcon\Mvc\View;
-
 class EventController extends ControllerBase{
-  
+
   public function beforeExecuteRoute(){ // function ที่ทำงานก่อนเริ่มการทำงานของระบบทั้งระบบ
 	  if(!$this->session->has('memberAuthen')) // ตรวจสอบว่ามี session การเข้าระบบ หรือไม่
-    		 $this->response->redirect('authen');   
-   } 
-   public function indexAction(){
-	
+         $this->response->redirect('authen');  
+   }
+ 
+  public function indexAction(){
+    $events=Activities::find();
+    $this->view->data=$events;
   }
+
   public function editAction(){
+    $id=$this->dispatcher->getParam("id");
+    $event=Activities::findFirst("id=".$id);
+    $this->view->data=$event;
     if($this->request->isPost()){
       $name = trim($this->request->getPost('name')); // รับค่าจาก form
       $date = trim($this->request->getPost('date')); // รับค่าจาก form
       $detail = trim($this->request->getPost('detail')); // รับค่าจาก form
-      $picture = trim($this->request->getPost('image')); 
-      $id=$this->session->get('id');
-      $event=Activities::findFirst("id = '$id'");
-      $event->id=$this->session->get('memberAuthen');
-      $event->name=$name;
-      $event->date=$date;
-      $event->detail=$detail;
-      $event->picture=$picture;
-      $event->save();
-      $this->response->redirect('event');
-      }
+      $image = trim($this->request->getPost('image')); // รับค่าจาก form
+    $event->name=$name;
+    $event->date=$date;
+    $event->detail=$detail;
+    $event->image=$image;
+    $event->save();
+    
+    $this->flashSession->success('Succeed');
+    }
   }
+
   public function addAction(){
     if($this->request->isPost()){
       $name = trim($this->request->getPost('name')); // รับค่าจาก form
       $date = trim($this->request->getPost('date')); // รับค่าจาก form
       $detail = trim($this->request->getPost('detail')); // รับค่าจาก form
-      $picture = trim($this->request->getPost('image')); 
-      
-      $event=new Activities();
-      $event->id=$this->session->get('memberAuthen');
-      $event->name=$name;
-      $event->date=$date;
-      $event->detail=$detail;
-      $event->picture=$picture;
-      $event->save();
-      $this->response->redirect('event');
-      }
+      $image = trim($this->request->getPost('image')); // รับค่าจาก form
+
+    $event=new Activities();
+    $event->name=$name;
+    $event->date=$date;
+    $event->detail=$detail;
+    $event->image=$image;
+    $event->save();
+    
+    $this->flashSession->success('Succeed');
+    $this->response->redirect('event');
+    }
   }
-  public function setIdAction($temp){
-    $this->session->set('id',$temp);
-	  $this->response->redirect('event/edit');    
-  }
-  public function deleteIdAction($temp){
-    $event = Activities::findFirst($temp);
+
+  public function delAction(){
+    $id=$this->dispatcher->getParam("id");
+    $event=Activities::findFirst("id=".$id);
     $event->delete();
-	  $this->response->redirect('event');    
+    $this->response->redirect('event');
   }
-  }
+}  
